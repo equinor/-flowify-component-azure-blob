bff77f6c327adfe4f698b0a4e7f5a3ca7f37a184
Adam Cheng <52572642+adamchengtkc@users.noreply.github.com>
2022-10-25 09:51:41 +0200

    fix arg parse seperator

--- src/args.js
+++ src/args.js
@@ -6,34 +6,34 @@ export function parse() {
     let arg = args[i].split('=');
     switch (arg[0]) {
       case 'auth_method':
-        process.env.AUTH_METHOD = arg[1];
+        process.env.AUTH_METHOD = arg.slice(1,);
         break;
       case 'container_name':
-        process.env.CONTAINER_NAME = arg[1];
+        process.env.CONTAINER_NAME = arg.slice(1,);
         break;
       case 'account':
-        process.env.ACCOUNT = arg[1];
+        process.env.ACCOUNT = arg.slice(1,);
         break;
       case 'ops':
-        process.env.OPS = arg[1];
+        process.env.OPS = arg.slice(1,);
         break;
       case 'download_path':
-        process.env.DOWNLOAD_PATH = arg[1];
+        process.env.DOWNLOAD_PATH = arg.slice(1,);
         break;
       case 'dir_filter':
-        process.env.DIR_FILTER = arg[1];
+        process.env.DIR_FILTER = arg.slice(1,);
         break;
       case 'file_filter':
-        process.env.FILE_FILTER = arg[1];
+        process.env.FILE_FILTER = arg.slice(1,);
         break;
       case 'upload_path':
-        process.env.UPLOAD_PATH = arg[1];
+        process.env.UPLOAD_PATH = arg.slice(1,);
         break;
       case 'upload_files':
-        process.env.UPLOAD_FILES = arg[1];
+        process.env.UPLOAD_FILES = arg.slice(1,);
         break;
       case 'file_upload_name':
-        process.env.FILE_UPLOAD_NAME =arg[1];
+        process.env.FILE_UPLOAD_NAME =arg.slice(1,);
       default:
     }
   }
